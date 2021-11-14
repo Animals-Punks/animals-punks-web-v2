e5d9d55a4cd4843647c0cbc7972e8740a86aa753
mo-zza <bigsmalljoe@gmail.com>
2021-11-14 18:38:16 +0900

    feat: ✨ lint

--- src/domains/User/components/RarityBox/RarityBox.impl.tsx
+++ src/domains/User/components/RarityBox/RarityBox.impl.tsx
@@ -1,51 +1,26 @@
 import { css } from "@emotion/react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 
 import {
     InformationBox,
     TitleText,
     ImageBox,
-    SelectBox,
     Button,
     TextFieldBox,
 } from "@common/components";
 import { useRarity } from "@User/hooks";
-import {
-    backgroundState,
-    speciesState,
-    headState,
-    eyesState,
-    mouthState,
-    clothesState,
-    accessoryState,
-} from "@common/services/recoil/itemAtom";
 import { searchRarityNumberState } from "@common/services/recoil/updateNameState";
 
 const RarityBox: React.FC = () => {
-    const background = useRecoilValue(backgroundState);
-    const species = useRecoilValue(speciesState);
-    const head = useRecoilValue(headState);
-    const eyes = useRecoilValue(eyesState);
-    const mouth = useRecoilValue(mouthState);
-    const clothes = useRecoilValue(clothesState);
-    const accessory = useRecoilValue(accessoryState);
     const setSearchRarityNumberState = useSetRecoilState(
         searchRarityNumberState
     );
 
-    const { onClick, onClickApName, imageUrlState, apNameState } = useRarity(
-        background,
-        species,
-        head,
-        eyes,
-        mouth,
-        clothes,
-        accessory
-    );
+    const { onClickApName, imageUrlState, apNameState } = useRarity();
 
     const onChangeNumber = (event: any) => {
         setSearchRarityNumberState(event.target.value);
-    }
+    };
 
     return (
         <InformationBox backgroundColor="#45EF8B">
@@ -62,26 +37,18 @@ const RarityBox: React.FC = () => {
                                 </ImageBox>
                                 <div style={{ display: "flex" }}>
                                     <Button
-                                        width="150px"
-                                        height="35px"
-                                        onClick={onClick}
-                                        fontSize="1.5vw"
-                                    >
-                                        Search
-                                </Button>
-                                    <Button
                                         width="200px"
                                         height="35px"
                                         onClick={onClickApName}
                                         fontSize="1.5vw"
                                     >
-                                        Search Number
-                                </Button>
+                                        Search
+                                    </Button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div style={{ display: 'flex', marginTop: '2vh' }}>
+                    <div style={{ display: "flex", marginTop: "2vh" }}>
                         <TextFieldBox onChange={onChangeNumber}>
                             Ap Number
                         </TextFieldBox>
@@ -106,15 +73,6 @@ const filterBackgroundStyle = css`
     flex-direction: column;
     width: 80%;
     height: 70%;
-`;
-
-const filterElementContainer = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    width: 70vw;
-    margin-top: 2vh;
 `;
 
 export default RarityBox;
--- src/domains/User/hooks/useRarity.ts
+++ src/domains/User/hooks/useRarity.ts
@@ -4,16 +4,7 @@ import { userService } from "@User/services";
 import { useRecoilValue } from "recoil";
 import { searchRarityNumberState } from "@/common/services/recoil/updateNameState";
 
-export default function useRarity(
-    background: string[],
-    species: string[],
-    head: string[],
-    eyes: string[],
-    mouth: string[],
-    clothes: string[],
-    accessory: string[]
-): {
-    onClick: any;
+export default function useRarity(): {
     onClickApName: any;
     imageUrlState: string;
     apNameState: string;
@@ -21,29 +12,6 @@ export default function useRarity(
     const [imageUrlState, setImageUrlState] = useState("");
     const [apNameState, setApNameState] = useState("");
     const searchRarityNumber = useRecoilValue(searchRarityNumberState);
-
-    const onClick = async () => {
-        const findRarityResult = await userService.findRarity({
-            background: background[0].toLowerCase(),
-            species: species[0].toLowerCase(),
-            head: head[0].toLowerCase(),
-            eyes: eyes[0].toLowerCase(),
-            mouth: mouth[0].toLowerCase(),
-            clothes: clothes[0].toLowerCase(),
-            accessory: accessory[0].toLowerCase(),
-        });
-
-        if (findRarityResult === undefined) {
-            alert("Please enter all items.");
-            return;
-        }
-        if (findRarityResult === null) {
-            alert("There is not have any animals punks.");
-            return;
-        }
-        setImageUrlState(findRarityResult.imgUrl);
-        setApNameState(findRarityResult.name);
-    };
 
     const onClickApName = async () => {
         const findAp = await userService.getApByNumber(searchRarityNumber);
@@ -54,10 +22,9 @@ export default function useRarity(
         }
         setImageUrlState(findAp.imgUrl);
         setApNameState(findAp.name);
-    }
+    };
 
     return {
-        onClick,
         onClickApName,
         imageUrlState,
         apNameState,
