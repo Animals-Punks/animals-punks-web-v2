84d5e13ed940db5e27b8590c2114a46ddd71813d
mo-zza <bigsmalljoe@gmail.com>
2021-12-22 23:18:30 +0900

    feat: ✨ ticket minting is over

--- src/domains/User/services/user.service.ts
+++ src/domains/User/services/user.service.ts
@@ -346,48 +346,59 @@ export class UserService implements IUserService {
             let responseResult = false;
             if (ticketType === "gold") {
                 try {
-                    const trxResult = await new CaverJsService().mintGoldTicket(
-                        address,
-                        ticketNumber[0].currentTicketNumber
-                    );
-                    const data = {
-                        ticketType,
-                        address,
-                        imageUrl: imageUrl[0].url,
-                        ticketNumber: ticketNumber[0].currentTicketNumber,
-                        usedAps: property,
-                    };
-                    if (trxResult === true) {
-                        await axios.post(
-                            `${process.env.NEXT_PUBLIC_TICKET_SERVER}ticket/mint`,
-                            data
-                        );
+                    if (ticketNumber > 295) {
+                        alert("골드 티켓 민팅이 종료되었습니다!");
+                        throw new Error("골드 티켓 민팅이 종료되었습니다!");
+                    } else {
+                        const trxResult =
+                            await new CaverJsService().mintGoldTicket(
+                                address,
+                                ticketNumber[0].currentTicketNumber
+                            );
+                        const data = {
+                            ticketType,
+                            address,
+                            imageUrl: imageUrl[0].url,
+                            ticketNumber: ticketNumber[0].currentTicketNumber,
+                            usedAps: property,
+                        };
+                        if (trxResult === true) {
+                            await axios.post(
+                                `${process.env.NEXT_PUBLIC_TICKET_SERVER}ticket/mint`,
+                                data
+                            );
+                        }
+                        responseResult = true;
                     }
-                    responseResult = true;
                 } catch (error) {
                     alert(error);
                 }
             } else {
-                try {
-                    const trxResult =
-                        await new CaverJsService().mintDiamondTicket(
+                if (ticketNumber > 196) {
+                    alert("다이아 티켓 민팅이 종료되었습니다!");
+                    throw new Error("다이아 티켓 민팅이 종료되었습니다!");
+                } else {
+                    try {
+                        const trxResult =
+                            await new CaverJsService().mintDiamondTicket(
+                                address,
+                                ticketNumber[0].currentTicketNumber
+                            );
+                        const data = {
+                            ticketType,
                             address,
-                            ticketNumber[0].currentTicketNumber
-                        );
-                    const data = {
-                        ticketType,
-                        address,
-                        imageUrl: imageUrl[0].url,
-                        ticketNumber: ticketNumber[0].currentTicketNumber,
-                        usedAps: property,
-                    };
-                    if (trxResult === true) {
-                        await axios.post(
-                            `${process.env.NEXT_PUBLIC_TICKET_SERVER}ticket/mint`,
-                            data
-                        );
-                    }
-                    responseResult = true;
+                            imageUrl: imageUrl[0].url,
+                            ticketNumber: ticketNumber[0].currentTicketNumber,
+                            usedAps: property,
+                        };
+                        if (trxResult === true) {
+                            await axios.post(
+                                `${process.env.NEXT_PUBLIC_TICKET_SERVER}ticket/mint`,
+                                data
+                            );
+                        }
+                        responseResult = true;
+                }
                 } catch (error) {
                     alert(error);
                 }
