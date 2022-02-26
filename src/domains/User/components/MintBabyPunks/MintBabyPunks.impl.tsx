aeae994dc9fd6b9b3b84c2f8a7abe43c55595b6c
mo-zza <bigsmalljoe@gmail.com>
2022-02-26 17:22:37 +0900

    fix: 🐛 time calculate

--- src/domains/User/components/MintBabyPunks/MintBabyPunks.impl.tsx
+++ src/domains/User/components/MintBabyPunks/MintBabyPunks.impl.tsx
@@ -74,14 +74,14 @@ const MintBabyPunks: React.FC = () => {
         const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
         const kr_curr = new Date(utc + KR_TIME_DIFF);
 
-        const mintTime = new Date(2022, 2, 26, 19);
+        const mintTime = new Date(2022, 2, 26, 20);
         const firstMintTimeUtc =
             mintTime.getTime() + mintTime.getTimezoneOffset() * 60 * 1000;
         const firstMintKrCurr = new Date(firstMintTimeUtc + KR_TIME_DIFF);
         const mintDate = kr_curr.getDate() - firstMintKrCurr.getDate();
         const calMintTime = kr_curr.getHours() - firstMintKrCurr.getHours();
 
-        if (mintDate === 0 && calMintTime > 0) {
+        if (mintDate >= 0 && calMintTime >= 0) {
             return true;
         }
         return false;
